4a249492bcd2e55dcfc04ec59afa6dbd8c9c07a5
jibauch94 <jibba_000@10.200.128.62>
2017-03-14 11:31:42 +0100

    http://www.dummies.com/programming/java/javafx-binding-properties/

--- src/bindings/Main.java
+++ src/bindings/Main.java
@@ -24,7 +24,7 @@ import javafx.stage.Stage;
  * @author jibba_000
  */
 public class Main extends Application {
-    
+    //
      public static void main(String[] args)
     {
         launch(args);
